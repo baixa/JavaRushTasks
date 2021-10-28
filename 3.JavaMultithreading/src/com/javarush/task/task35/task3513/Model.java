b1ccee0fc902cd1e4697bbb263008c09f5a1a3d5
Ivan Maksimchuk <baixa2003@gmail.com>
2021-10-28 18:20:35 +0300

    Update big task -> Java Multithreading 10 уровень("2048") Добавил в класс Model реализацию заполнения игрового поля клетками

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task35.task3513;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Содержит игровую логику и хранит игровое поле
@@ -10,11 +11,40 @@ public class Model {
     private Tile[][] gameTiles;
 
     public Model() {
+        resetGameTiles();
+    }
+
+    //Создает новое игровое поле
+    protected void resetGameTiles() {
         gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
         for (int i = 0; i < gameTiles.length; i++) {
             for (int j = 0; j < gameTiles[i].length; j++) {
                 gameTiles[i][j] = new Tile();
             }
         }
+        addTile();
+        addTile();
+    }
+
+    //Добавляет на свободную клетку новую плитку со значением 2 или 4 (с соотношением 9 : 1)
+    private void addTile() {
+        List<Tile> emptyTiles = getEmptyTiles();
+        if (!emptyTiles.isEmpty()) {
+            int index = (int) (Math.random() * emptyTiles.size()) % emptyTiles.size();
+            Tile emptyTile = emptyTiles.get(index);
+            emptyTile.value = Math.random() < 0.9 ? 2 : 4;
+        }
+    }
+
+    //Метод возвращает пустые клетки
+    private List<Tile> getEmptyTiles() {
+        final List<Tile> result = new ArrayList<>();
+        for (int i = 0; i < gameTiles.length; i++) {
+            for (int j = 0; j < gameTiles[i].length; j++) {
+                if (gameTiles[i][j].isEmpty())
+                    result.add(gameTiles[i][j]);
+            }
+        }
+        return result;
     }
 }
