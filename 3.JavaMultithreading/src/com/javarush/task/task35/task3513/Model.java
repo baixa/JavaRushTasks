8877b1ce02fa3c5241e47cc193088f14076a06d4
Ivan Maksimchuk <baixa2003@gmail.com>
2021-10-28 18:04:24 +0300

    Update big task -> Java Multithreading 10 уровень("2048") Добавил в класс Model реализацию игрового поля

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -1,7 +1,20 @@
 package com.javarush.task.task35.task3513;
 
+import java.util.Arrays;
+
 /**
  * Содержит игровую логику и хранит игровое поле
  */
 public class Model {
+    private static final int FIELD_WIDTH = 4;
+    private Tile[][] gameTiles;
+
+    public Model() {
+        gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
+        for (int i = 0; i < gameTiles.length; i++) {
+            for (int j = 0; j < gameTiles[i].length; j++) {
+                gameTiles[i][j] = new Tile();
+            }
+        }
+    }
 }
