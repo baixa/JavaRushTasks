d432aef65464b9622207fee3510ee154183ae085
Ivan Maksimchuk <baixa2003@gmail.com>
2021-10-31 15:03:53 +0300

    Update big task -> Java Multithreading 10 уровень("2048") Добавил в класс Model реализацию слияния и сжатия клеток, а также возможность перемещать клетки влево

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task35.task3513;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -9,16 +10,18 @@ import java.util.List;
 public class Model {
     private static final int FIELD_WIDTH = 4;
     private Tile[][] gameTiles;
+    int maxTile = 0;
+    int score = 0;
 
     public Model() {
         resetGameTiles();
     }
 
     //Создает новое игровое поле
-    protected void resetGameTiles() {
+    void resetGameTiles() {
         gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
-        for (int i = 0; i < gameTiles.length; i++) {
-            for (int j = 0; j < gameTiles[i].length; j++) {
+        for (int i = 0; i < FIELD_WIDTH; i++) {
+            for (int j = 0; j < FIELD_WIDTH; j++) {
                 gameTiles[i][j] = new Tile();
             }
         }
@@ -38,13 +41,74 @@ public class Model {
 
     //Метод возвращает пустые клетки
     private List<Tile> getEmptyTiles() {
-        final List<Tile> result = new ArrayList<>();
-        for (int i = 0; i < gameTiles.length; i++) {
-            for (int j = 0; j < gameTiles[i].length; j++) {
-                if (gameTiles[i][j].isEmpty())
-                    result.add(gameTiles[i][j]);
+        final List<Tile> list = new ArrayList<>();
+        for (Tile[] tileArray : gameTiles) {
+            for (Tile t : tileArray)
+                if (t.isEmpty()) {
+                    list.add(t);
+                }
+        }
+        return list;
+    }
+
+    //Метод проводит сжатие плиток путем перемещения пустых плиток вправо
+    private boolean compressTiles(Tile[] tiles) {
+        int insertPosition = 0;
+        boolean isArrayChanged = false;
+        for (int i = 0; i < FIELD_WIDTH; i++) {
+            if (!tiles[i].isEmpty()) {
+                if (i != insertPosition) {
+                    tiles[insertPosition] = tiles[i];
+                    tiles[i] = new Tile();
+                    isArrayChanged = true;
+                }
+                insertPosition++;
             }
         }
-        return result;
+        return isArrayChanged;
+    }
+
+    //Метод проводит слияние плиток одного номинала
+    private boolean mergeTiles(Tile[] tiles) {
+        LinkedList<Tile> tilesList = new LinkedList<>();
+        boolean isArrayChanged = false;
+        for (int i = 0; i < FIELD_WIDTH; i++) {
+            if (tiles[i].isEmpty()) {
+                continue;
+            }
+
+            if (i < FIELD_WIDTH - 1 && tiles[i].value == tiles[i + 1].value) {
+                int updatedValue = tiles[i].value * 2;
+                if (updatedValue > maxTile) {
+                    maxTile = updatedValue;
+                }
+                score += updatedValue;
+                tilesList.addLast(new Tile(updatedValue));
+                tiles[i + 1].value = 0;
+                isArrayChanged = true;
+            } else {
+                tilesList.addLast(new Tile(tiles[i].value));
+            }
+            tiles[i].value = 0;
+        }
+
+        for (int i = 0; i < tilesList.size(); i++) {
+            tiles[i] = tilesList.get(i);
+        }
+        
+        return isArrayChanged;
+    }
+
+    //Метод вызывает для каждой строки методы сжатия и слияния и добавляет в случае необходимости одну плитку
+    public void left() {
+        boolean isNeedAddTile = false;
+        for (int i = 0; i < FIELD_WIDTH; i++) {
+            if (compressTiles(gameTiles[i]) | mergeTiles(gameTiles[i])) {
+                isNeedAddTile = true;
+            }
+        }
+        if (isNeedAddTile) {
+            addTile();
+        }
     }
 }
